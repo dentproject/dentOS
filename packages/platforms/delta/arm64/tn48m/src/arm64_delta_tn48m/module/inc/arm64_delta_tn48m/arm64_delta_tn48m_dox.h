d5a1463cbd280c5f68fb0fde7115a21b8153039c
chenglin-tsai <chenglin.tsai@deltaww.com>
2020-08-17 10:33:52 +0800

    Add new platforms: delta tn48m series
    Signed-off-by: chenglin-tsai <chenglin.tsai@deltaww.com>

---
+++ packages/platforms/delta/arm64/tn48m/src/arm64_delta_tn48m/module/inc/arm64_delta_tn48m/arm64_delta_tn48m_dox.h
@@ -0,0 +1,46 @@
+/************************************************************
+ * <bsn.cl fy=2016 v=onl>
+ *
+ *        Copyright 2020 Delta Networks, Inc.
+ *
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ * 
+ * </bsn.cl>
+ ************************************************************
+ *
+ *
+ *
+ ***********************************************************/
+
+/********************************************************//**
+ *
+ * arm64_delta_tn48m Doxygen Header
+ *
+ ***********************************************************/
+#ifndef __ARM64_DELTA_TN48M_DOX_H__
+#define __ARM64_DELTA_TN48M_DOX_H__
+
+/**
+ * @defgroup arm64_delta_tn48m arm64_delta_tn48m - onlpsim Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup arm64_delta_tn48m-arm64_delta_tn48m Public Interface
+ * @defgroup arm64_delta_tn48m-config Compile Time Configuration
+ * @defgroup arm64_delta_tn48m-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __ARM64_DELTA_TN48M_DOX_H__ */
---
+++ packages/platforms/delta/arm64/tn48m/tn48m2/platform-config/r0/src/python/arm64_delta_tn48m2_r0/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from onl.platform.base import *
+from onl.platform.delta import *
+
+class OnlPlatform_arm64_delta_tn48m2_r0(OnlPlatformDelta,
+                                         OnlPlatformPortConfig_48x1_4x10):
+    PLATFORM='arm64-delta-tn48m2-r0'
+    MODEL="TN48M2"
+    SYS_OBJECT_ID=".48.13"
+
+    def baseconfig(self):
+        self.insmod('optoe')
+
+        # Insert platform drivers
+        self.insmod("arm64-delta-tn48m-cpld.ko")
+        self.insmod("arm64-delta-tn48m-led.ko")
+
+        ########### initialize I2C bus 0 ###########
+        self.new_i2c_devices (
+            [
+                ('tn48m_cpld',    0x41, 0),
+            ]
+        )
+
+        ########### initialize I2C bus 1 ###########
+        self.new_i2c_devices (
+            [
+                # FAN Controller
+                ('adt7473', 0x2e, 1),
+
+                # Temperature devices
+                ('tmp1075', 0x4a, 1),
+                ('tmp1075', 0x4b, 1),
+
+                # System EEPROM
+                ('24c64', 0x56, 1),
+            ]
+        )
+
+        # Set SFP port name
+        subprocess.call('echo port49 > /sys/bus/i2c/devices/3-0050/port_name', shell=True)
+        subprocess.call('echo port50 > /sys/bus/i2c/devices/4-0050/port_name', shell=True)
+        subprocess.call('echo port51 > /sys/bus/i2c/devices/5-0050/port_name', shell=True)
+        subprocess.call('echo port52 > /sys/bus/i2c/devices/6-0050/port_name', shell=True)
+
+        return True
