e4f1854485a538dd7cbea2e5401b6282eecc0084
Steven Noble <snoble@sonn.com>
2020-12-09 15:48:08 -0800

    Update __init__.py

--- packages/platforms/delta/arm64/tn48m/tn48m-poe/platform-config/r0/src/python/arm64_delta_tn48m_poe_r0/__init__.py
+++ packages/platforms/delta/arm64/tn48m/tn48m-poe/platform-config/r0/src/python/arm64_delta_tn48m_poe_r0/__init__.py
@@ -35,10 +35,6 @@ class OnlPlatform_arm64_delta_tn48m_poe_r0(OnlPlatformDelta,
             ]
         )
 
-        # Insert prestera kernel modules
-        os.system("insmod /lib/modules/`uname -r`/kernel/drivers/net/ethernet/marvell/prestera_sw/prestera_sw.ko")
-        os.system("insmod /lib/modules/`uname -r`/kernel/drivers/net/ethernet/marvell/prestera_sw/prestera_pci.ko")
-
         # Insert prestera kernel module
         self.insmod("prestera_sw.ko")
         self.insmod("prestera_pci.ko")
