d6c100d0604bf70d5b28a666f80ff026d5727418
Cary Chen <cary.sc.chen@deltaww.com>
2023-05-08 16:42:41 +0800

    [tn48m] Add new platform TN48M2-SWDEV
     - Add new platform support for TN48M2-SWDEV to Dent-3.0.
       This Platform is modified from TN48M2 with the SPI flash changed to 64MB to accommodate switchdev supported ONIE.
    
    Signed-off-by: Cary Chen <cary.sc.chen@deltaww.com>

---
+++ packages/platforms/delta/arm64/tn48m/tn48m2-swdev/platform-config/r0/src/python/arm64_delta_tn48m2_swdev_r0/__init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+from onl.platform.base import *
+from onl.platform.delta import *
+
+class OnlPlatform_arm64_delta_tn48m2_swdev_r0(OnlPlatformDelta,
+                                         OnlPlatformPortConfig_48x1_4x10):
+    PLATFORM='arm64-delta-tn48m2-swdev-r0'
+    MODEL="TN48M2-SWDEV"
+    SYS_OBJECT_ID=".48.14"
+
+    def baseconfig(self):
+        # Insert platform drivers
+        self.insmod("arm64-delta-tn48m-cpld.ko")
+        self.insmod("arm64-delta-tn48m-led.ko")
+
+        ########### initialize I2C bus 1 ###########
+        self.new_i2c_devices (
+            [
+                # FAN Controller
+                ('adt7473', 0x2e, 1),
+
+                # Temperature devices
+                ('tmp1075', 0x4a, 1),
+                ('tmp1075', 0x4b, 1),
+            ]
+        )
+
+        # Insert Marvell prestera modules by only probing prestera_pci module
+        # Suggested by Taras Chornyi
+        self.modprobe('prestera_pci')
+
+        # set up systemctl rules
+        for swp in range(1, 53):
+           cmd = "systemctl enable switchdev-online@swp%d" % swp
+        subprocess.check_call(cmd, shell=True)
+
+        return True
